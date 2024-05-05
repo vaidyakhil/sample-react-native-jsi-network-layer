8698fbac4cc408e1b3c9866ca5f8681ac7a8980b
Akhil Vaidya <vaidyakhil@gmail.com>
2024-05-05 20:06:36 +0530

    use custom spy method in place of default

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
  * @format
  */
 
+if (__DEV__) {
+  const customSpyMethod = info => {
+    if (
+      ['WebSocketModule', 'UIManager', 'NativeAnimatedModule'].includes(
+        info.module,
+      )
+    ) {
+      return;
+    }
+    console.log(
+      `🕵️ : ${info.type === MessageQueue.TO_JS ? 'N->JS' : 'JS->N'} : ` +
+        `${info.module != null ? info.module + '.' : ''}${info.method}`,
+    );
+  };
+  const MessageQueue = require('react-native/Libraries/BatchedBridge/MessageQueue');
+  MessageQueue.spy(customSpyMethod);
+}
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
